b3f44250c894f3ff0ca21579746d5cc4b2d514f7
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2025-07-02 12:48:25 +0330

    finish db and requirements

--- 5_project (weblog)/weblog/settings.py
+++ 5_project (weblog)/weblog/settings.py
@@ -83,12 +83,34 @@ WSGI_APPLICATION = "weblog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "weblog_db_for_aparat",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "weblog_db_for_aparat_with_postgres",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "127.0.0.1",  # or "localhost"
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
